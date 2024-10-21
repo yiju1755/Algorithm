e7f519868502f827d12aa1cf110967f27b5ea7f7
yiju1755 <129149450+yiju1755@users.noreply.github.com>
2024-10-22 00:00:27 +0900

    [D1] Title: 대각선 출력하기, Time: 115 ms, Memory: 17,144 KB -BaekjoonHub

---
+++ SWEA/D1/2027. 대각선 출력하기/대각선 출력하기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+        for(int i=0; i<5; i++){
+            for(int j=0; j<5; j++){
+                if(i==j){System.out.print("#");}
+                else{System.out.print("+");}
+            }
+            System.out.println("");
+        }
+	}
+}
