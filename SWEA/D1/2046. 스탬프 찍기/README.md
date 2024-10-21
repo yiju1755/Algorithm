1a12c2bd151e2fbd5a4426ad3c100e6ba479a240
yiju1755 <129149450+yiju1755@users.noreply.github.com>
2024-10-21 19:39:43 +0900

    [D1] Title: 스탬프 찍기, Time: 106 ms, Memory: 19,156 KB -BaekjoonHub

---
+++ SWEA/D1/2046. 스탬프 찍기/README.md
@@ -0,0 +1,15 @@
+# [D1] 스탬프 찍기 - 2046 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5QKdT6AyYDFAUq) 
+
+### 성능 요약
+
+메모리: 19,156 KB, 시간: 106 ms, 코드길이: 336 Bytes
+
+### 제출 일자
+
+2024-10-21 19:39
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D1/2046. 스탬프 찍기/스탬프 찍기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        for(int i=0; i<a; i++)
+        {
+            System.out.print('#');
+        }
+	}
+}
