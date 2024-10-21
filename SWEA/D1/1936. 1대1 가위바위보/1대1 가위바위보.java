a7253ecf48f59d5be257825607d07f86de9c4079
yiju1755 <129149450+yiju1755@users.noreply.github.com>
2024-10-21 21:36:34 +0900

    [D1] Title: 1대1 가위바위보, Time: 117 ms, Memory: 16,080 KB -BaekjoonHub

---
+++ SWEA/D1/1936. 1대1 가위바위보/1대1 가위바위보.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        
+        if(a>b){
+            System.out.print("A");
+        }
+        else {
+            System.out.print("B");
+        }
+	}
+}
