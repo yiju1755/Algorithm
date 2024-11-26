3e790bb505aac1983a4e12e49517047742f2f8fe
yiju1755 <129149450+yiju1755@users.noreply.github.com>
2024-11-26 21:18:32 +0900

    [Bronze V] Title: 개, Time: 96 ms, Memory: 14048 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.print("||_/=\\\\__|");
+    }
+}
