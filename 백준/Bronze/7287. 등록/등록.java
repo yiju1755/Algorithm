ff6d68952c5d3909722355a7584d7284d8418fc0
yiju1755 <129149450+yiju1755@users.noreply.github.com>
2024-11-26 21:09:36 +0900

    [Bronze V] Title: 등록, Time: 100 ms, Memory: 13868 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("2");
+        System.out.println("yiju1755");
+    }
+}
