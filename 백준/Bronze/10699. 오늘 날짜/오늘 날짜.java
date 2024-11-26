7e4061581eaddbffd3b839626cb52ea34175d7f0
yiju1755 <129149450+yiju1755@users.noreply.github.com>
2024-11-26 21:01:40 +0900

    [Bronze V] Title: 오늘 날짜, Time: 172 ms, Memory: 17112 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+import java.time.*;
+
+public class Main {
+    public static void main(String[] args){
+        LocalDate nowDate = LocalDate.now();
+        System.out.print(nowDate);
+    }
+}
