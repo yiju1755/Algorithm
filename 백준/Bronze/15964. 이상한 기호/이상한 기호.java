3eb3ef25d89d71b61e1ce44e435b31c1cec56b6a
yiju1755 <129149450+yiju1755@users.noreply.github.com>
2024-12-06 19:51:27 +0900

    [Bronze V] Title: 이상한 기호, Time: 100 ms, Memory: 14104 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        System.out.print(cal(A,B));
+    }
+    public static int cal(int a, int b) {
+        return (a+b)*(a-b);
+    }
+}
