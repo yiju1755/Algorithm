ae35ec01d25c3d35afa234b0c9fa3575b9faaa11
yiju1755 <129149450+yiju1755@users.noreply.github.com>
2025-04-24 21:58:19 +0900

    [Bronze II] Title: 나머지, Time: 100 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Set<Integer> modSet = new HashSet<>();
+
+        for(int i = 0; i<10; i++){
+            modSet.add(Integer.parseInt(br.readLine())%42);
+        }
+
+        System.out.println(modSet.size());
+
+    }
+}
